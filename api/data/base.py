180fac4fe650ad20ceb3db15a471b317cd9d0ed9
BodeMurairi2 <b.murairi@alustudent.com>
2025-10-07 16:14:18 +0200

    Analyzis

---
+++ api/data/base.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from sqlalchemy.orm import declarative_base
+
+# Base class for all models
+Base = declarative_base()
---
+++ api/data/save_vendor.sql
@@ -0,0 +1,9 @@
+INSERT INTO vendors (vendor_id, vendor_name)
+VALUES 
+    (1, 'Creative Mobile Technologies'),
+    (2, 'VeriFone Inc'),
+    (3, 'UNKNOWN'),
+    (4, 'UNKWOWN'),
+    (5, 'UNKWOWN'),
+    (6, 'UNKWOWN'),
+    (7, 'UNKWOWN');
